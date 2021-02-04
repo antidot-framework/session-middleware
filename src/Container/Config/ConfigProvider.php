7ec50542242f136829f81c81d5adfa5c677151ba
Koldo Picaza <dev@antidotfw.io>
2021-02-04 23:54:49 +0100

    add php 8 support

--- src/Container/Config/ConfigProvider.php
+++ src/Container/Config/ConfigProvider.php
@@ -8,6 +8,9 @@ use Antidot\Session\Application\Http\Middleware\SessionMiddleware;
 
 class ConfigProvider
 {
+    /**
+     * @return array<string, array<string, string>>
+     */
     public function __invoke(): array
     {
         return [
